810499e521dce8c3d04c0992866c4cf0eca33214
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-22 20:42:17 +0300

    Associate post with like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,3 @@
 class Like < ApplicationRecord
+  belongs_to :post, class_name: "post", foreign_key: "post_id"
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user, class_name: "user", foreign_key: "author_id"
   has_many :comments, class_name: "comment", foreign_key: "post_id"
+  has_many :likes, class_name: "like", foreign_key: "reference_id"
 end
