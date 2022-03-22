60c137bbbc17233e3ca787ce338c684de6572bdf
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-22 20:46:28 +0300

    Associate user with like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :post, class_name: "post", foreign_key: "post_id"
+  belongs_to :user, class_name: "user", foreign_key: "author_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_173943) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_174249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_173943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
@@ -54,5 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_173943) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
