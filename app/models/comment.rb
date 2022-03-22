b67c7b0855aee3164a611b1eec9fab85438ca757
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-22 21:29:00 +0300

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: "user", foreign_key: "author_id"
-  belongs_to :post, class_name: "post", foreign_key: "post_id"
+  belongs_to :user, class_name: "User"
+  belongs_to :post
 end
