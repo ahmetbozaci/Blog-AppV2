b0acb2d134532c41dd583420d04bc93ab7ab40dc
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-22 20:03:08 +0300

    Associate user model with post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  belongs_to :user, class_name: "user", foreign_key: "author_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :posts, class_name: "post", foreign_key: "author_id"
 end
