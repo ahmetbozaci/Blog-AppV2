dd75b9f747dd8d6df4fbdef11d7b3280e1b9252e
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-22 20:27:51 +0300

    Associate user model to comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_165651) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_170633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_165651) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -45,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_165651) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
