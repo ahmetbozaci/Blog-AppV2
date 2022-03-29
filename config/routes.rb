86a02c10cf24041e023f21cacc8001a2b4b7ef66
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-30 02:16:07 +0300

    Update user index for show users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show] do
     end
-  end 
+  end
+  root to: 'users#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ ActiveRecord::Base.connection.tables.each do |t|
 end
 
 first_user = User.create(name: 'Tom', 
-  photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
+  photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
   bio: 'Teacher from Mexico.')
 second_user = User.create(name: 'Lilly', 
-  photo: 'https://unsplash.com/photos/F_-0BxGuVvo', 
+  photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
   bio: 'Teacher from Poland.')
 
   first_post = Post.create(author: first_user, title: 'Post 1', text: 'This is my first post')
