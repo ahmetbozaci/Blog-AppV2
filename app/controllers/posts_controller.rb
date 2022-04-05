80e612244890c4117c0502d414ff43a858487767
Ahmet Bozaci <ahmt9417@gmail.com>
2022-04-05 06:56:50 +0300

    Add comments on the pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     @posts = User.find(params[:user_id]).posts
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
